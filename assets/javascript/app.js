7cd6c118473b19109d96cfd74f208be5ab0c089a
christinegillis <chrisa8300@gmail.com>
2017-03-10 02:47:46 -0500

    finalizes js and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//keywords to render buttons at start of app
 topics = ["eye-roll", "whatever", "excited", "annoyed", "mad"];
 
 function generateButton(){
@@ -10,7 +11,6 @@ for(var i = 0; i < topics.length; i++){
 	generateButton();
 }
 
-
 $(document).on("click", "button", function() {
 
 	var buttonVal = $(this).text();
@@ -18,28 +18,58 @@ $(document).on("click", "button", function() {
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
 	buttonVal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-	console.log("this is the value of the button: " + buttonVal);
+//make api call using text on button
 	$.ajax({
 		url: queryURL,
 		method: "GET",
 	}).done(function (response){
-		console.log(response.data[0].images);
+		//prevents buildup of gifs, refreshes when button is pressed
 		$("#giphy-holder").empty();
+
+		//loops through response obj that was returned 		
 		for(var i = 0; i < response.data.length; i++){
- 		
+
 	 		var img = $("<img>").attr({
 	            "id": "Image-" + i,
 	            "src": response.data[i].images.fixed_height_still.url,
 	            "alt": buttonVal,
-	        }).appendTo("#giphy-holder");		
- 		}
+	            //to handle pausing and replaying gifs
+	            "data-animated": response.data[i].images.fixed_height.url,
+	            "data-still": response.data[i].images.fixed_height_still.url,
+	            "data-state": "still",
 
-		// $("#giphy-holder").html(response);
+	        }).appendTo("#giphy-holder");
+
+	 		//adds rating to the bottom of ea image
+	 		var rating = $("<p>" + response.data[i].rating + "</p>");
+	 		$("#giphy-holder").append(rating);
+		};
+
 	}).fail(function() {
 		console.log("something went wrong");
 	});
 
 });
 
+//<img src="http...gif" data-animated="http...gif" data-state="still">
 
-// generateButton();
+//pauses and replays gifs depending on data-state
+$(document).on("click", "img", function() {
+	if ($(this).attr("data-state") === "still") {
+        $(this).attr("src", $(this).attr("data-animated"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+});
+
+/*takes user input and creates dynamic button, 
+button will then make api call bc of element name */
+$(document).on("click", "#user-button", function() {
+	var userMood = $("#user-input").val().trim();
+
+	var userButton = $("<button>");
+	userButton.text(userMood);
+	$("#button-holder").append(userButton);
+});
